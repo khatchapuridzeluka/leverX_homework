1887fb81aa8c24fee566e29f5287fffc2e5c067b
Luka khachapuridze <khatchapuridzeluka91@gmail.com>
2025-05-18 00:22:34 +0400

    Chess Models have been added: Player, Game, Opening, Tournament

--- leverX/Program.cs
+++ leverX/Program.cs
@@ -6,12 +6,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
