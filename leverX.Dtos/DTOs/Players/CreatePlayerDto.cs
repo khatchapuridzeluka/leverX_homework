6a3d753a61d2399d0a442ce7f60ca51001baead4
khatchapuridzeluka <khatchapuridzeluka91@gmail.com>
2025-06-04 19:33:33 +0400

    move validation into its class

--- leverX.Dtos/DTOs/Players/CreatePlayerDto.cs
+++ leverX.Dtos/DTOs/Players/CreatePlayerDto.cs
@@ -1,9 +1,35 @@
-﻿using leverX.Dtos.DTOs.Players;
+﻿using FluentValidation;
+using leverX.Dtos.DTOs.Players;
 
 namespace leverX.DTOs.Players
 {
     public record CreatePlayerDto : BasePlayerDto
     {
+        public class Validator : AbstractValidator<CreatePlayerDto>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.Name)
+              .NotEmpty().WithMessage("Name is required.")
+              .MaximumLength(50);
+
+                RuleFor(x => x.LastName)
+                    .NotEmpty().WithMessage("Last name is required.")
+                    .MaximumLength(50);
+
+                RuleFor(x => x.FideRating)
+                    .InclusiveBetween(100, 3000).WithMessage("FIDE rating must be between 100 and 3000.");
+
+                RuleFor(x => x.Nationality)
+                    .IsInEnum().WithMessage("Invalid nationality.");
+
+                RuleFor(x => x.Sex)
+                    .IsInEnum().WithMessage("Invalid sex.");
+
+                RuleFor(x => x.Title)
+                    .IsInEnum().WithMessage("Invalid title.");
+            }
+        }
     }
 
 }
